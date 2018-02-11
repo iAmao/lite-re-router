e9446a8b36f633350fd210148f51b85b882ba5e7
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2018-02-11 13:09:49 +0100

    feat: add anchor links and default 404 page

---
+++ src/FourOhFour.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default () => {
+  return (
+    <div style={{
+      position: 'absolute',
+      textAlign: 'center',
+      top: '45vh',
+      width: '100vw'
+    }}>
+      <h2>404!</h2>
+      <h3>Page not found</h3>
+    </div>
+  )
+}
---
+++ src/__tests__/Back.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { shallow } from 'enzyme';
+
+import Back from '../Back';
+
+
+describe('Back', () => {
+    describe('Snapshot Test', () => {
+        it('renders a link', () => {
+            const tree = renderer.create(
+                <Back>Button</Back>
+            ).toJSON();
+            expect(tree).toMatchSnapshot();
+        });
+    });
+
+    describe('Unit Test', () => {
+        it('should change context on click', () => {
+            let count = 0;
+            const wrapper = shallow(Back(
+                { children: 'Button' },
+                { location: { back: () => count++}}
+            ));
+
+            wrapper.simulate('click');
+
+            expect(count).toEqual(1);
+            expect(wrapper.text()).toEqual('Button');
+        })
+    });
+});
