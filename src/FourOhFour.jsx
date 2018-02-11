fa113a18f37e45af2cd5c443179f4e7b07912f7b
Inumidun Amao <inumidunamao@Inumiduns-MacBook-Pro.local>
2018-02-11 13:14:20 +0100

    add option to go back on 404 page

--- src/FourOhFour.jsx
+++ src/FourOhFour.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Back from './Back';
 
 
 export default () => {
@@ -11,6 +12,17 @@ export default () => {
     }}>
       <h2>404!</h2>
       <h3>Page not found</h3>
+      <br />
+      <Back
+        style={{
+          color: '#fff',
+          cursor: 'pointer',
+          padding: '10px 20px',
+          backgroundColor: '#333'
+        }}
+      >
+        Go Back
+      </Back>
     </div>
   )
 }
